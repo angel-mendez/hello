8312f17e18cec1ce7d6b621ab9ed2c78434f2ebb
Angel <amendezlopez2@huskers.unl.edu>
2020-08-26 00:58:02 +0000

    initial commit

--- hello.c
+++ hello.c
@@ -1,1 +1,17 @@
+/**
+ * Author: Angel Mendez lopez
+ *
+ * Date: 2020/08/18
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
 
+int main(int argc, char **argv) {
+
+  printf("Angel Mendez Lopez\n");
+
+  return 0;
+}
